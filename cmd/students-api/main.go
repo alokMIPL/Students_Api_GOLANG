a5bf2129c3fa372fc4a9be5eec69a31767eb3612
alokMIPL <alok@madhulata.com>
2026-09-12 15:11:33 +0530

    code added

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/alokMIPL/students-api/internal/config"
 )
 
 func main() {
+
+	fmt.Println("Welconme to students api")
 
 	// Load config
 
@@ -16,9 +19,26 @@ func main() {
 	log.Println("Environment", cfg.Env)
 
 	// database setup
+
 	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welocome to students api"))
+	})
+
 	// setup server
 
-	fmt.Println("Welconme to students api")
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("Server started %s", cfg.Addr)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("failed to start server")
+	}
 
 }
