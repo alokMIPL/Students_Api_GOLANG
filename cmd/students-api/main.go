581f63c2e68f7f4feacd98a5afd96a9ce80330e9
alokMIPL <alok@madhulata.com>
2026-09-15 10:27:17 +0530

    saperate get section code added

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/alokMIPL/students-api/internal/config"
+	"github.com/alokMIPL/students-api/internal/http/handlers/student"
 )
 
 func main() {
@@ -27,9 +28,7 @@ func main() {
 	// setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to students api"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 
 	// setup server
 	server := http.Server{
@@ -38,8 +37,6 @@ func main() {
 	}
 
 	slog.Info("Server started", slog.String("address", cfg.Addr))
-
-	// fmt.Println("Server stared as = ", cfg.HTTPServer.Addr)
 
 	done := make(chan os.Signal, 1)
 
